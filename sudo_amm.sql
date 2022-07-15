b96336354a3e9e4f2c763706b696a0458cc853f8
0xRob <0xRobin5@gmail.com>
2022-07-15 14:56:49 +0200

    use eth traces

--- sudo_amm.sql
+++ sudo_amm.sql
@@ -1,78 +1,77 @@
 WITH swaps as (
 SELECT
     "call_block_time",
-    ("swapListEntry"->'maxCost')::bigint as "maxCost",
-    CONCAT('\x',substring(("swapListEntry"->'swapInfo'->>'pair')::text from 3))::bytea as "pair",
-    array_agg(id::int ORDER BY id ASC) as "nftIds",
-    "tx_hash",
-    "swapListEntry"
-FROM(
-SELECT
-    jsonb_array_elements("swapList") as "swapListEntry",
-    "tx_hash","call_block_time"
-FROM    (
-    SELECT "swapList","call_tx_hash" as "tx_hash","call_block_time" FROM
-    (
-        SELECT *
-        FROM sudo_amm."LSSVMRouter_call_robustSwapETHForSpecificNFTs"
-    ) foo
-    WHERE "call_success" = 'true'
-        ) foo
-    ) foo
-    LEFT JOIN LATERAL jsonb_array_elements("swapListEntry"->'swapInfo'->'nftIds') id ON true
-    GROUP BY call_block_time, "swapListEntry", "tx_hash"
+    t."from",
+    "call_tx_hash" as "tx_hash"
+FROM sudo_amm."LSSVMRouter_call_robustSwapETHForSpecificNFTs" s
+LEFT JOIN (SELECT * FROM ethereum.transactions WHERE "to" = '\x2B2e8cDA09bBA9660dCA5cB6233787738Ad68329') t
+ON t."hash" = s."call_tx_hash"
 )
 
-,hashes AS (SELECT DISTINCT "tx_hash" FROM swaps)
-
-,eth_payments AS (
-SELECT "tx_hash", "from", "to", sum("value") as "value"
-FROM ethereum.traces
-WHERE "tx_hash" in (select * from hashes) AND "value" > 0
-GROUP BY "tx_hash", "from", "to"
+,eth_transfers AS (
+SELECT t."tx_hash", t."from", t."to", sum(t."value") as "value"
+FROM ethereum.traces t
+INNER JOIN swaps s
+ON s."tx_hash" = t."tx_hash"
+WHERE "tx_success" AND "success" AND "call_type" = 'call'
+GROUP BY t."tx_hash", t."from", t."to"
 )
 
 ,pairs AS (
-SELECT "nft_contract", "pair", "asset_recipient", "fee_recipient" FROM
-    (SELECT distinct "poolAddress" FROM sudo_amm."LSSVMPairFactory_evt_NewPair") evt
-    LEFT JOIN (
-   SELECT "_nft" as nft_contract,
-        "output_pair" as "pair",
-        CASE WHEN "_assetRecipient" = '\x0000000000000000000000000000000000000000' THEN "output_pair" ELSE "_assetRecipient" END as "asset_recipient",
-        "contract_address" as "fee_recipient"
-        FROM sudo_amm."LSSVMPairFactory_call_createPairETH" WHERE "call_success" = 'true'
-    ) call
-    ON evt."poolAddress" = call."pair"
-),
+SELECT "_nft" as "nft_contract",
+    "output_pair" as "pair",
+    (CASE WHEN "_assetRecipient" = '\x0000000000000000000000000000000000000000' THEN "output_pair" ELSE "_assetRecipient" END) as "asset_recipient",
+    "contract_address" as "fee_recipient"
+ FROM
+    sudo_amm."LSSVMPairFactory_call_createPairETH" call
+    WHERE "call_success" = 'true'
+)
 
-transfers AS (
+,eth_pool_transfers  AS (
+SELECT "tx_hash", "pair",
+    coalesce("payment",0) - coalesce("refund", 0) as "amount",
+    coalesce("fees", 0) as "fees"
+FROM (
+    SELECT "tx_hash", "pair",
+        sum("value") filter (WHERE "to" = "pair") as "payment",
+        sum("value") filter (WHERE "from" = "pair" AND "to" = "fee_recipient") as "fees",
+        sum("value") filter (WHERE "from" = "pair" AND "to" <> "fee_recipient" AND "to" <> "asset_recipient") as "refund"
+    FROM eth_transfers t
+    INNER JOIN pairs p
+    ON "from" = p."pair" OR "to" = p."pair"
+    GROUP BY "tx_hash", "pair"
+) foo
+)
+
+,transfers AS (
 SELECT t."evt_tx_hash", t."from", t."contract_address", array_agg(t."tokenId"::int ORDER BY t."tokenId" ASC) as "transferIds"
 FROM erc721."ERC721_evt_Transfer" t
-WHERE t."evt_tx_hash" in (select * from hashes)
+WHERE t."evt_tx_hash" in (select DISTINCT "tx_hash" from swaps)
 GROUP BY t."evt_tx_hash", t."contract_address", t."from"
 )
 
-,all_swaps AS (
-SELECT s.*,
-    t."transferIds",
-    array(select unnest(s."nftIds") except select unnest(t."transferIds")) as "unfilled",
-    array(select unnest(s."nftIds") intersect select unnest(t."transferIds")) as "filled"
-FROM swaps s
-LEFT JOIN transfers t
-ON s.tx_hash = t.evt_tx_hash and s.pair = t."from"
+,swap_details AS (
+SELECT
+    s."call_block_time" as "block_time",
+    t."contract_address" as "nft_contract_address",
+    t."transferIds" as "nft_token_ids_array",
+    cardinality(t."transferIds") as "number_of_items",
+    s."from" as "buyer",
+    p."asset_recipient" as "seller",
+    p."pair" as "pair_address",
+    e."amount" as "eth_amount_raw",
+    e."fees" as "eth_fees_raw",
+    s."tx_hash"
+FROM
+transfers t
+INNER JOIN eth_pool_transfers e
+ON t."evt_tx_hash" = e."tx_hash" AND t."from" = e."pair"
+LEFT JOIN swaps s
+ON e."tx_hash" = s."tx_hash"
+LEFT JOIN pairs p
+ON e."pair" = p."pair"
+ORDER BY s."call_block_time"
 )
 
-,full_filled_swaps AS (
-SELECT * FROM all_swaps
-WHERE cardinality(unfilled) = 0
-)
 
-,partial_filled_swaps AS (
-SELECT * FROM all_swaps
-WHERE cardinality(unfilled) > 0
-)
-
--- SELECT s.*, t.value FROM swaps s LEFT JOIN eth_transactions t ON s.tx_hash  = t.hash
-select * FROM full_filled_swaps
-
-
+select * FROM swap_details
