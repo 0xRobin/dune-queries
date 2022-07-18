5b6047e060dfa8b4dc6eb74503dd6f0a56a15a3a
0xRob <0xRobin5@gmail.com>
2022-07-18 08:53:16 +0200

    right-click-commit

---
+++ art_blocks/RCS.sql
@@ -0,0 +1,118 @@
+WITH attests AS (
+    select block_time,
+    concat('x',substring("data"::text from 71 for 4))::bit(16) ::int as "id"
+    from ethereum.transactions
+    where "to" = '\xd16809c0a7d82c9e7552a01fd608fff90efb564f'
+    AND substring("data"::text from 2 for 9) = 'xf414a308'
+    AND "block_time" < '{{date}}'
+)
+
+, attested AS (
+    select "id",
+    max(block_time) as "last_attested",
+    case when (max(block_time) - '{{date}}') < interval '1 year' then true ELSE false END as attest_valid
+    from attests
+    group by "id"
+)
+
+, all_ids AS(
+    select (id+1) as "id",
+    id::int % 32::int as col,
+    id::int / 32::int as row
+    from (
+    select * from generate_series(0, 1023) as id
+    ) foo
+)
+
+, colors AS (
+ select * from
+ (VALUES
+ (0, '7739d1','🟪'),
+ (1, '7ec292','🟩'),
+ (2, '22133d','⬛') ,
+ (3, 'e14d9c','🟥'),
+ (4, 'ffffff','⬜')
+) AS t (id, hex, emoji)
+)
+
+, magic AS (
+select
+row_number() over () as "id",
+"magic"
+from (
+    select
+    unnest("column2") as "magic"
+    FROM (
+        select * from (VALUES
+        ('array', ARRAY[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+        1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2,
+        2, 2, 2, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+        1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1, 2, 3, 2, 1, 2, 3, 1, 1, 2, 2, 2,
+        2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 2, 3, 3, 3, 2, 1, 3, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 3, 3, 3, 3, 1, 3, 3, 3, 3, 1, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
+        2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2,
+        2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2,
+        2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 2, 2, 2, 2, 2,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0]))
+        AS foo
+        ) foo
+    ) foo
+)
+
+, pixels AS (
+select f.id, col, row, f."color_id"
+from(
+    select
+     id.*,
+     case when coalesce(a."attest_valid",false) then m."magic" else 4 END as "color_id"
+     from all_ids id
+     LEFT JOIN attested a
+     ON a."id" = id."id" and a."attest_valid"
+     LEFT JOIN magic m
+     ON m."id" = id."id"
+) f
+)
+
+, color_channels AS (
+ select
+ y."id" as "row",
+ x."id" as "column",
+ c."id" as "color",
+ 10*(y."id"::int) + c."id" as "chann_order",
+ concat((y."id"),'_',col."hex") as "channel"
+ from
+ (select * from generate_series(0, 32) as id) y
+ cross join
+ (select * from generate_series(0, 32) as id) x
+ cross join
+ (select * from generate_series(0, 4) as id) c
+ left join colors col
+ ON c."id" = col."id"
+ order by chann_order DESC
+)
+
+SELECT "channel", "column", "signal" FROM
+(
+    SELECT
+    c."channel",
+    c."column",
+    c."chann_order",
+    case when p."id" > 0 THEN 1 ELSE 0 END as signal
+    FROM color_channels c
+    LEFT JOIN pixels p
+    ON c.row = p.row AND c."column" = p."col" AND p."color_id" = c."color"
+) sig
+WHERE "signal" = 1 OR "column" = 0
+ORDER BY "chann_order" DESC
